34b6f08d5265e9ebc2bfeb2ae963849f5f5f43f5
raheelanwaar03 <raheelanwaar0316@gmail.com>
2026-02-02 16:05:10 +0100

    team page added

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,5 +1,120 @@
 @extends('layout.app')
 
 @section('content')
-
 @endsection
+<section class="promo-sec position-relative jarallax" style="background-image: url('{{ asset('assets/images/promo-sec.jpg') }}');">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="promo-txt">
+                    <h1 class="text-uppercase text-info mt-3 mb-5 text-center">Team Players</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"> Team</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Promo Section End -->
+
+<!-- Team Section Start -->
+<section class="team-page sec-padding">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-4 col-sm-6">
+                <div class="team-member text-center">
+                    <div class="team-img mb-4">
+                        <img class="img-fluid" src="{{ asset('assets/images/team-m1.png') }}" alt="">
+                    </div>
+                    <h3 class="text-uppercase mb-0"><a href="single-player.html">Felix Nightshade</a></h3>
+                    <p>Football Coach</p>
+                    <div class="social-share">
+                        <a href="#"><i class="feather-icon icon-twitter"></i></a>
+                        <a href="#"><i class="feather-icon icon-linkedin"></i></a>
+                        <a href="#"><i class="feather-icon icon-instagram"></i></a>
+                        <a href="#"><i class="feather-icon icon-facebook"></i></a>
+                    </div>
+                </div>
+            </div><!-- Team Member End -->
+            <div class="col-lg-4 col-sm-6">
+                <div class="team-member text-center">
+                    <div class="team-img mb-4">
+                        <img class="img-fluid" src="{{ asset('assets/images/team-m5.png') }}" alt="">
+                    </div>
+                    <h3 class="text-uppercase mb-0"><a href="single-player.html">Angelo Nightshade</a></h3>
+                    <p>Football Coach</p>
+                    <div class="social-share">
+                        <a href="#"><i class="feather-icon icon-twitter"></i></a>
+                        <a href="#"><i class="feather-icon icon-linkedin"></i></a>
+                        <a href="#"><i class="feather-icon icon-instagram"></i></a>
+                        <a href="#"><i class="feather-icon icon-facebook"></i></a>
+                    </div>
+                </div>
+            </div><!-- Team Member End -->
+            <div class="col-lg-4 col-sm-6">
+                <div class="team-member text-center">
+                    <div class="team-img mb-4">
+                        <img class="img-fluid" src="{{ asset('assets/images/team-m7.png') }}" alt="">
+                    </div>
+                    <h3 class="text-uppercase mb-0"><a href="single-player.html">Josua Blackwood</a></h3>
+                    <p>Football Coach</p>
+                    <div class="social-share">
+                        <a href="#"><i class="feather-icon icon-twitter"></i></a>
+                        <a href="#"><i class="feather-icon icon-linkedin"></i></a>
+                        <a href="#"><i class="feather-icon icon-instagram"></i></a>
+                        <a href="#"><i class="feather-icon icon-facebook"></i></a>
+                    </div>
+                </div>
+            </div><!-- Team Member End -->
+            <div class="col-lg-4 col-sm-6">
+                <div class="team-member text-center">
+                    <div class="team-img mb-4">
+                        <img class="img-fluid" src="{{ asset('assets/images/team-m2.png') }}" alt="">
+                    </div>
+                    <h3 class="text-uppercase mb-0"><a href="single-player.html">Xavier Blackwood</a></h3>
+                    <p>Football Coach</p>
+                    <div class="social-share">
+                        <a href="#"><i class="feather-icon icon-twitter"></i></a>
+                        <a href="#"><i class="feather-icon icon-linkedin"></i></a>
+                        <a href="#"><i class="feather-icon icon-instagram"></i></a>
+                        <a href="#"><i class="feather-icon icon-facebook"></i></a>
+                    </div>
+                </div>
+            </div><!-- Team Member End -->
+            <div class="col-lg-4 col-sm-6">
+                <div class="team-member text-center">
+                    <div class="team-img mb-4">
+                        <img class="img-fluid" src="{{ asset('assets/images/team-m3.png') }}" alt="">
+                    </div>
+                    <h3 class="text-uppercase mb-0"><a href="single-player.html">Xavier Blackwood</a></h3>
+                    <p>Football Coach</p>
+                    <div class="social-share">
+                        <a href="#"><i class="feather-icon icon-twitter"></i></a>
+                        <a href="#"><i class="feather-icon icon-linkedin"></i></a>
+                        <a href="#"><i class="feather-icon icon-instagram"></i></a>
+                        <a href="#"><i class="feather-icon icon-facebook"></i></a>
+                    </div>
+                </div>
+            </div><!-- Team Member End -->
+            <div class="col-lg-4 col-sm-6">
+                <div class="team-member text-center">
+                    <div class="team-img mb-4">
+                        <img class="img-fluid" src="{{ asset('assets/images/team-m8.png') }}" alt="">
+                    </div>
+                    <h3 class="text-uppercase mb-0"><a href="single-player.html">Russel Arnold</a></h3>
+                    <p>Football Coach</p>
+                    <div class="social-share">
+                        <a href="#"><i class="feather-icon icon-twitter"></i></a>
+                        <a href="#"><i class="feather-icon icon-linkedin"></i></a>
+                        <a href="#"><i class="feather-icon icon-instagram"></i></a>
+                        <a href="#"><i class="feather-icon icon-facebook"></i></a>
+                    </div>
+                </div>
+            </div><!-- Team Member End -->
+        </div>
+    </div>
+</section>
