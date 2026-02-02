ec1de108fdbd44eb389dd429b802845e4129ac33
raheelanwaar03 <raheelanwaar0316@gmail.com>
2026-02-02 21:51:16 +0100

    working on layout and nav

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,8 @@
+
+
+ <script src="{{ asset('assets/admin//libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+ <script src="{{ asset('assets/admin//libs/simplebar/simplebar.min.js') }}"></script>
+ <script src="{{ asset('assets/admin//js/app.js') }}"></script>
+ </body>
+
+ </html>
