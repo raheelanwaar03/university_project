cc004615c0da278b83a899e759715c6c08c2d6ac
raheelanwaar03 <raheelanwaar0316@gmail.com>
2026-02-03 00:17:22 +0100

    contact details added

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -10,8 +10,8 @@
                         <span class="icon rounded-pill"><i class="feather-icon icon-phone"></i></span>
                         <h3>Call Us</h3>
                         <ul class="list-unstyled">
-                            <li><a href="tel:8390065439288">+8 - 3900 6543 9288 </a></li>
-                            <li><a href="tel:8390065439287">+8 - 2340 9837 9009</a></li>
+                            <li><a href="tel:123456789">+39 - 123456789 </a></li>
+                            <li><a href="tel:123456789">+39 - 123456789</a></li>
                         </ul>
                     </div>
                     <!-- Card End -->
@@ -22,17 +22,11 @@
                         <h3>Email Us</h3>
                         <ul class="list-unstyled">
                             <li>
-                                <a
-                                    href="https://html.theme-village.com/cdn-cgi/l/email-protection#136a7c66617e727a7f53747e727a7f3d707c7e"><span
-                                        class="__cf_email__"
-                                        data-cfemail="cdb4a2b8bfa0aca4a18daaa0aca4a1e3aea2a0">[email&#160;protected]</span>
+                                <a href="#"><span class="__cf_email__">champions1@gmail.com</span>
                                 </a>
                             </li>
                             <li>
-                                <a
-                                    href="https://html.theme-village.com/cdn-cgi/l/email-protection#6b12041e19060a02070a0f0f190e18182b0c060a020745080406"><span
-                                        class="__cf_email__"
-                                        data-cfemail="3a494a55484e49524f4e4e7a5d575b535614595557">[email&#160;protected]</span></a>
+                                <a href="#"><span class="__cf_email__">champions2@gmail.com</span></a>
                             </li>
                         </ul>
                     </div>
@@ -42,7 +36,9 @@
                     <div class="contact-card text-center rounded-3">
                         <span class="icon rounded-pill"><i class="feather-icon icon-map-pin"></i></span>
                         <h3>Locations</h3>
-                        <p>785 15h Street – Sydney Harbor Bridge Circular City of Sydney, #Australia.</p>
+                        <p>Via Antonio Fogazzaro 58.
+                            Postal code: 30172
+                        </p>
                     </div>
                     <!-- Card End -->
                 </div>
