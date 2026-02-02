6cbd0051839a350228ab92c9a59cc3017c02dd72
raheelanwaar03 <raheelanwaar0316@gmail.com>
2026-02-02 19:37:14 +0100

    admin middleware created and defined

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\admin;
 use App\Http\Middleware\status;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(
             [
                 'status' => status::class,
+                'admin' => admin::class,
             ]
         );
     })
