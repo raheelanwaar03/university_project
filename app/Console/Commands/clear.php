cb2546ddd6acba5571476ba3047be67fb3b1db52
raheelanwaar03 <raheelanwaar0316@gmail.com>
2026-02-02 19:24:35 +0100

    inserted code added

--- app/Console/Commands/clear.php
+++ app/Console/Commands/clear.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 
 class clear extends Command
 {
@@ -11,20 +14,40 @@ class clear extends Command
      *
      * @var string
      */
-    protected $signature = 'app:clear';
+    protected $signature = 'make:clean';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'To insert user and admin';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        Artisan::call('migrate:fresh');
+        Artisan::call('cache:clear');
+        Artisan::call('config:clear');
+        Artisan::call('view:clear');
+        Artisan::call('route:clear');
+
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make('asdfasdf');
+        $user->status = 'active';
+        $user->role = 'admin';
+        $user->save();
+
+        $user = new User();
+        $user->name = 'User';
+        $user->email = 'user@gmail.com';
+        $user->password = Hash::make('asdfasdf');
+        $user->status = 'active';
+        $user->role = 'User';
+        $user->save();
     }
 }
