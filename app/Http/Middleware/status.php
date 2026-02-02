5c018c8aff4db5fab66cec9800d33a7045a59e59
raheelanwaar03 <raheelanwaar0316@gmail.com>
2026-02-02 19:26:06 +0100

    status will be active

--- app/Http/Middleware/status.php
+++ app/Http/Middleware/status.php
@@ -15,7 +15,7 @@ class status
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->user()->role == 'approved') {
+        if (auth()->user()->role == 'active') {
             return $next($request);
         } else {
             return redirect()->url('/')->with('error', 'Please wait for Admin approval');
